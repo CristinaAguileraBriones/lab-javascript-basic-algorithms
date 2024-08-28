0b276f2768f879713deff0124cd79e4c82817531
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-08-28 16:52:56 +0200

    primera copia

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // Iteration 1: Names and Input
+const hacker2 = "Clara"
+const hacker1 = "Cristina"
 
+console.log(`The driver´s name is ${hacker1} `)
+console.log(`The navigator´s name is ${hacker2} `)
 
 // Iteration 2: Conditionals
 
+if(hacker1.length>hacker2.length){
+
+
+
+    console.log("The driver has the longest name, it has " + hacker1.length + " characters")
+}else if(hacker1.length<hacker2.length){
+
+    console.log("The driver has the longest name, it has " + hacker2.length + " characters")
+}else {
+
+    console.log("Wow, you both have equally long names, XX characters!")
+}
+
 
 // Iteration 3: Loops
+
+
